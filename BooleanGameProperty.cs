33ae775a6e7e3a46810d1652f0e385bf56593259
Vincent Crowe <vincent.crowe@emishealth.com>
2019-06-07 13:53:31 +0100

    Added implicit mutator group

--- BooleanGameProperty.cs
+++ BooleanGameProperty.cs
@@ -37,4 +37,14 @@ namespace VinlaTech.Unity
             }
         }
     }    
+
+    public class ImplicitMutatorGroup : MonoBehaviour
+    {
+        public void ApplyAll()
+        {
+            var mutators = gameObject.GetComponents<GamePropertyMutator>();
+            foreach(var mutator in mutators)
+                mutator.ApplyMutation();
+        }
+    }
 }
